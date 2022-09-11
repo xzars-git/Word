7a8c997223cb824e36fc38ef302bcc5f455ec096
Arsenius Purbandono <xzars@upi.edu>
2022-09-11 23:14:36 +0700

    Recycler View Udah Bisa DI Click tpi msih toast

--- app/src/main/java/com/hustle/historiapreunership/Adapter/DetailedWordAdapter.kt
+++ app/src/main/java/com/hustle/historiapreunership/Adapter/DetailedWordAdapter.kt
@@ -11,13 +11,29 @@ import com.hustle.historiapreunership.R
 class DetailedWordAdapter (private val detailedList:ArrayList<Word>) :
     RecyclerView.Adapter<DetailedWordAdapter.DetailedViewHolder>() {
 
-    class DetailedViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+    private lateinit var mListener : onItemClickListener
+
+    interface onItemClickListener{
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: onItemClickListener){
+        mListener = listener
+    }
+
+    class DetailedViewHolder(itemView : View, listener: onItemClickListener) : RecyclerView.ViewHolder(itemView) {
         val word : TextView = itemView.findViewById(R.id.word)
+
+        init {
+            itemView.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailedViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.each_word, parent, false)
-        return DetailedViewHolder(view)
+        return DetailedViewHolder(view,mListener)
     }
 
     override fun onBindViewHolder(holder: DetailedViewHolder, position: Int) {
--- app/src/main/java/com/hustle/historiapreunership/MainActivity.kt
+++ app/src/main/java/com/hustle/historiapreunership/MainActivity.kt
@@ -2,9 +2,20 @@ package com.hustle.historiapreunership
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.hustle.historiapreunership.Adapter.DetailedWordAdapter
+import com.hustle.historiapreunership.Model.Word
 import com.hustle.historiapreunership.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var adapter : DetailedWordAdapter
+    private lateinit var recView: RecyclerView
+    private lateinit var wordArrayList : ArrayList<Word>
+    private lateinit var abjad : ArrayList<String>
 
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +23,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //make the launcher screen as first fragment
+        abjad = arrayListOf(
+            getString(R.string.A),
+            getString(R.string.B),
+            getString(R.string.C),
+            getString(R.string.D),
+            getString(R.string.E),
+            getString(R.string.F),
+            getString(R.string.G),
+            getString(R.string.H),
+            getString(R.string.I),
+            getString(R.string.J)
+        )
 
-        supportFragmentManager.beginTransaction().replace(R.id.Fragment_vertical_words, vertical_Word()).commit()
+        recView = findViewById(R.id.recView)
+        recView.layoutManager = LinearLayoutManager(this)
+        recView.setHasFixedSize(true)
+
+        wordArrayList = arrayListOf<Word>()
+
+        for(i in abjad.indices){
+            val word = Word(abjad[i])
+            wordArrayList.add(word)
+        }
+
+        var adapter = DetailedWordAdapter(wordArrayList)
+        recView.adapter = adapter
+        adapter.setOnItemClickListener(object : DetailedWordAdapter.onItemClickListener{
+            override fun onItemClick(position: Int) {
+                Toast.makeText(this@MainActivity, "You Click on Item no $position",Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
